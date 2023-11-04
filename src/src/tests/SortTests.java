591f1b2d1bfa48d3ffcdece2145bdecb85ca7065
Faisal Hossain <faisal.hossain75@gmail.com>
2023-11-04 00:38:18 -0500

    Add bubble sort test cases

--- src/src/tests/SortTests.java
+++ src/src/tests/SortTests.java
@@ -11,17 +11,55 @@ public class SortTests {
 
     @Test
     public void testBubbleSort1() {
-
         Integer[] input = {3, 2, 4};
         Integer[] expected = {2, 3, 4};
 
         bubbleSort(input, Comparator.naturalOrder());
-
         assertArrayEquals(expected, input);
     }
 
     @Test
     public void testBubbleSort2() {
-        // do something
+        Integer[] input = {1, 2, 3};
+        Integer[] expected = {1, 2, 3};
+
+        bubbleSort(input, Comparator.naturalOrder());
+        assertArrayEquals(expected, input);
+    }
+
+    @Test
+    public void testBubbleSort3() {
+        Integer[] input = {3, 2, 1};
+        Integer[] expected = {1, 2, 3};
+
+        bubbleSort(input, Comparator.naturalOrder());
+        assertArrayEquals(expected, input);
+    }
+
+    @Test
+    public void testBubbleSort4() {
+        Integer[] input = {3, 3, 1};
+        Integer[] expected = {1, 3, 3};
+
+        bubbleSort(input, Comparator.naturalOrder());
+        assertArrayEquals(expected, input);
+    }
+
+    @Test
+    public void testBubbleSort5() {
+        Integer[] input = {-1, 2, 1};
+        Integer[] expected = {-1, 1, 2};
+
+        bubbleSort(input, Comparator.naturalOrder());
+        assertArrayEquals(expected, input);
+    }
+
+    @Test
+    public void testBubbleSort6() {
+        Integer[] input = {};
+        Integer[] expected = {};
+
+        bubbleSort(input, Comparator.naturalOrder());
+        assertArrayEquals(expected, input);
     }
 }
