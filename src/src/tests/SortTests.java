10d141649c90acd0e465166682f93c70bfa15663
Faisal Hossain <faisal.hossain75@gmail.com>
2023-11-04 14:26:32 -0500

    Add selection sort test cases

--- src/src/tests/SortTests.java
+++ src/src/tests/SortTests.java
@@ -4,8 +4,8 @@ import java.util.Comparator;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Assertions;
-import static iterative_sorts.Sorting.bubbleSort;
-import static iterative_sorts.Sorting.insertionSort;
+
+import static iterative_sorts.Sorting.*;
 
 
 public class SortTests {
@@ -122,6 +122,63 @@ public class SortTests {
             Integer[] expected = {};
 
             insertionSort(input, Comparator.naturalOrder());
+            Assertions.assertArrayEquals(expected, input);
+        }
+    }
+
+    @Nested
+    class SelectionSortTests {
+        @Test
+        public void testSelectionSort1() {
+            Integer[] input = {3, 2, 4};
+            Integer[] expected = {2, 3, 4};
+
+            selectionSort(input, Comparator.naturalOrder());
+            Assertions.assertArrayEquals(expected, input, "Actual: " + Arrays.toString(input) + ", Expected: " + Arrays.toString(expected));
+        }
+
+        @Test
+        public void testSelectionSort2() {
+            Integer[] input = {1, 2, 3};
+            Integer[] expected = {1, 2, 3};
+
+            selectionSort(input, Comparator.naturalOrder());
+            Assertions.assertArrayEquals(expected, input, "Actual: " + Arrays.toString(input) + ", Expected: " + Arrays.toString(expected));
+        }
+
+        @Test
+        public void testSelectionSort3() {
+            Integer[] input = {3, 2, 1};
+            Integer[] expected = {1, 2, 3};
+
+            selectionSort(input, Comparator.naturalOrder());
+            Assertions.assertArrayEquals(expected, input, "Actual: " + Arrays.toString(input) + ", Expected: " + Arrays.toString(expected));
+        }
+
+        @Test
+        public void testSelectionSort4() {
+            Integer[] input = {3, 3, 1};
+            Integer[] expected = {1, 3, 3};
+
+            selectionSort(input, Comparator.naturalOrder());
+            Assertions.assertArrayEquals(expected, input);
+        }
+
+        @Test
+        public void testSelectionSort5() {
+            Integer[] input = {-1, 2, 1};
+            Integer[] expected = {-1, 1, 2};
+
+            selectionSort(input, Comparator.naturalOrder());
+            Assertions.assertArrayEquals(expected, input);
+        }
+
+        @Test
+        public void testSelectionSort6() {
+            Integer[] input = {};
+            Integer[] expected = {};
+
+            selectionSort(input, Comparator.naturalOrder());
             Assertions.assertArrayEquals(expected, input);
         }
     }
