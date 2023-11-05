f0dc8a0906a81ab4e4e372aea0c19c91160f81bd
Faisal Hossain <faisal.hossain75@gmail.com>
2023-11-04 19:42:11 -0500

    Improved naming of bubble sort test cases

--- src/src/tests/SortTests.java
+++ src/src/tests/SortTests.java
@@ -1,8 +1,6 @@
 package tests;
 import java.util.Arrays;
 import java.util.Comparator;
-
-import org.junit.BeforeClass;
 import org.junit.jupiter.api.*;
 import static iterative_sorts.Sorting.*;
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -26,67 +24,94 @@ public class SortTests {
     class BubbleSortTests {
 
         @Test
-        public void sortedArrayTest() {
+        public void testSortedArray() {
             Integer[] input = {1, 2, 3};
             Integer[] expected = {1, 2, 3};
-
+            int expectedCount = input.length - 1;
             bubbleSort(input, comparator);
-            assertEquals(2, comparator.getCount());
+
+            // Sort Test
             Assertions.assertArrayEquals(expected, input, "Actual: " + Arrays.toString(input) + ", Expected: " + Arrays.toString(expected));
+            // Comparison Count Test
+            assertEquals(expectedCount, comparator.getCount());
         }
 
         @Test
-        public void sortedArrayComparisonsTest() {
-            Integer[] input = {1, 2, 3};
-            Integer[] expected = {1, 2, 3};
+        public void testReverseSortedArray() {
+            Integer[] input = {5, 4, 3, 2, 1};
+            Integer[] expected = {1, 2, 3, 4, 5};
+            int expectedCount = input.length * (input.length - 1) / 2; // The number of comparisons will be n * (n-1) / 2 for a reversed array of size n
+            bubbleSort(input, comparator);
 
-            bubbleSort(input, Comparator.naturalOrder());
+            // Sort Test
             Assertions.assertArrayEquals(expected, input, "Actual: " + Arrays.toString(input) + ", Expected: " + Arrays.toString(expected));
+            // Comparison Count Test
+            assertEquals(expectedCount, comparator.getCount());
         }
 
         @Test
-        public void testBubbleSort2() {
-            Integer[] input = {3, 2, 4};
-            Integer[] expected = {2, 3, 4};
+        public void testAllSameElementsArray() {
+            Integer[] input = {4, 4, 4};
+            Integer[] expected = {4, 4, 4};
+            int expectedCount = input.length - 1;
+            bubbleSort(input, comparator);
 
-            bubbleSort(input, Comparator.naturalOrder());
+            // Sort Test
             Assertions.assertArrayEquals(expected, input, "Actual: " + Arrays.toString(input) + ", Expected: " + Arrays.toString(expected));
+            // Comparison Count Test
+            assertEquals(expectedCount, comparator.getCount());
         }
 
         @Test
-        public void testBubbleSort3() {
-            Integer[] input = {3, 2, 1};
-            Integer[] expected = {1, 2, 3};
+        public void testDuplicateElementsArray() {
+            Integer[] input = {5, 4, 3, 2, 1, 1};
+            Integer[] expected = {1, 1, 2, 3, 4, 5};
+            int expectedCount = input.length * (input.length - 1) / 2;
+            bubbleSort(input, comparator);
 
-            bubbleSort(input, Comparator.naturalOrder());
+            // Sort Test
             Assertions.assertArrayEquals(expected, input, "Actual: " + Arrays.toString(input) + ", Expected: " + Arrays.toString(expected));
+            // Comparison Count Test
+            assertEquals(expectedCount, comparator.getCount());
         }
 
         @Test
-        public void testBubbleSort4() {
-            Integer[] input = {3, 3, 1};
-            Integer[] expected = {1, 3, 3};
-
-            bubbleSort(input, Comparator.naturalOrder());
-            Assertions.assertArrayEquals(expected, input);
-        }
-
-        @Test
-        public void testBubbleSort5() {
-            Integer[] input = {-1, 2, 1};
-            Integer[] expected = {-1, 1, 2};
-
-            bubbleSort(input, Comparator.naturalOrder());
-            Assertions.assertArrayEquals(expected, input);
-        }
-
-        @Test
-        public void testBubbleSort6() {
+        public void testEmptyArray() {
             Integer[] input = {};
             Integer[] expected = {};
+            int expectedCount = 0;
+            bubbleSort(input, comparator);
 
-            bubbleSort(input, Comparator.naturalOrder());
-            Assertions.assertArrayEquals(expected, input);
+            // Sort Test
+            Assertions.assertArrayEquals(expected, input, "Actual: " + Arrays.toString(input) + ", Expected: " + Arrays.toString(expected));
+            // Comparison Count Test
+            assertEquals(expectedCount, comparator.getCount());
+        }
+
+        @Test
+        public void testNegativeElementArray() {
+            Integer[] input = {-1, 2, 1};
+            Integer[] expected = {-1, 1, 2};
+            int expectedCount = input.length * (input.length - 1) / 2;
+            bubbleSort(input, comparator);
+
+            // Sort Test
+            Assertions.assertArrayEquals(expected, input, "Actual: " + Arrays.toString(input) + ", Expected: " + Arrays.toString(expected));
+            // Comparison Count Test
+            assertEquals(expectedCount, comparator.getCount());
+    }
+
+        @Test
+        public void testSingleElementArray() {
+            Integer[] input = {1};
+            Integer[] expected = {1};
+            int expectedCount = 0;
+            bubbleSort(input, comparator);
+
+            // Sort Test
+            Assertions.assertArrayEquals(expected, input, "Actual: " + Arrays.toString(input) + ", Expected: " + Arrays.toString(expected));
+            // Comparison Count Test
+            assertEquals(expectedCount, comparator.getCount());
         }
     }
 
@@ -204,4 +229,3 @@ public class SortTests {
         }
     }
 }
-
